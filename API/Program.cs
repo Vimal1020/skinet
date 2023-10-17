b89122b51a041e69cbb2cf5a994fd46a70bbbf3e
Vimal1020 <vimalmessi107@gmail.com>
2023-10-17 17:27:51 +0530

    Added IdentityServices and login logout using it

--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,24 @@
+using System.Runtime.CompilerServices;
 using API.Extensions;
 using API.Helpers;
 using API.Middleware;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
-// Build the host
-
-// Add services to the container.
 
 builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(connectionString));
+var config = builder.Configuration;
+builder.Services.AddDbContext<StoreContext>
+(x => x.UseSqlite(config.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AppIdentityDbContext>
+(x => x.UseSqlite(config.GetConnectionString("IdentityConnection")));
+
 builder.Services.AddSingleton<IConnectionMultiplexer>(c => 
 {
     var configure = ConfigurationOptions.Parse
@@ -21,6 +27,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
 });
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 builder.Services.AddApplicationServices();
+builder.Services.AddIdentityServices(config);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerDocumentation();
 // Add services to the container.
@@ -47,6 +54,11 @@ using (var scope = app.Services.CreateScope())
         var context = services.GetRequiredService<StoreContext>();
         await context.Database.MigrateAsync();
         await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+        var userManager = services.GetRequiredService<UserManager<AppUser>>();
+        var IdentityContext = services.GetRequiredService<AppIdentityDbContext>();
+        await IdentityContext.Database.MigrateAsync();
+        await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
     }
     catch (Exception ex)
     {
@@ -66,6 +78,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseCors("AllowOrigin");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
