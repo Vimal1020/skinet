f99095c194cb2d9481df005b98f2b0dc8a28d6be
Vimal1020 <vimalmessi107@gmail.com>
2023-10-19 17:20:07 +0530

    Identity(adding token) swagger changes and Validation

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,14 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$\r\n",
+        ErrorMessage ="Password must have 1 Uppercase, 1 Lowercase, 1 Number, 1 non alphanumeric and atleats 6 characters")]
         public string Password { get; set; }
     }
 }
